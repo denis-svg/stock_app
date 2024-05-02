e4ecb5fafce1ad483164262ca8ec84866475d293
deniska <dprodan289@gmail.com>
2024-05-02 21:14:45 +0300

    rechart

--- src/components/weather_app/DisplayWeather.jsx
+++ src/components/weather_app/DisplayWeather.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
+import { AreaChart, Area, XAxis, Tooltip } from "recharts";
 
 function DisplayWeather({ weatherForecast }) {
+  if (weatherForecast == null)
+    return (
+      <>
+        <h1>Input a weather forecast</h1>
+      </>
+    );
   const [selectedDay, setSelectedDay] = useState(null);
 
   // Organize forecast data by day
@@ -25,6 +32,37 @@ function DisplayWeather({ weatherForecast }) {
     }
   };
 
+  // Get day of the week
+  const getDayOfWeek = (dateString) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const date = new Date(dateString);
+    return days[date.getDay()];
+  };
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      const forecast = payload[0].payload;
+      return (
+        <div className="custom-tooltip">
+          <p>Time: {forecast.dt_txt.split(" ")[1]}</p>
+          <p>Temperature: {forecast.main.temp}°C</p>
+          <p>Feels like: {forecast.main.feels_like}°C</p>
+          <p>Description: {forecast.weather[0].description}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   const daysData = organizeDataByDay();
 
   return (
@@ -32,18 +70,31 @@ function DisplayWeather({ weatherForecast }) {
       <h1>Weather Forecast</h1>
       {Object.keys(daysData).map((day) => (
         <div key={day} onClick={() => handleDayClick(day)}>
-          <h2>{day}</h2>
+          <h2>
+            {getDayOfWeek(day)}, {day}
+          </h2>
           {selectedDay === day && (
-            <div>
-              {daysData[day].map((forecast) => (
-                <div key={forecast.dt}>
-                  <p>Time: {forecast.dt_txt.split(" ")[1]}</p>
-                  <p>Temperature: {forecast.main.temp}°C</p>
-                  <p>Feels like: {forecast.main.feels_like}°C</p>
-                  <p>Description: {forecast.weather[0].description}</p>
-                </div>
-              ))}
-            </div>
+            <AreaChart
+              width={600}
+              height={100}
+              data={daysData[day].map((item) => ({
+                ...item,
+                dt_txt: item.dt_txt.slice(0, -3),
+              }))}
+            >
+              <Area
+                type="monotone"
+                dataKey="main.temp"
+                stroke="#ffc602"
+                fill="#fcda66"
+              />
+              <XAxis
+                dataKey="dt_txt"
+                tickFormatter={(timeStr) => timeStr.split(" ")[1]}
+                padding={{ left: 20, right: 20 }}
+              />
+              <Tooltip content={<CustomTooltip />} />
+            </AreaChart>
           )}
         </div>
       ))}
