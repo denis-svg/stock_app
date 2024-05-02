e18339df56754fb5dfc689c5e4494f2e8ed500f2
deniska <dprodan289@gmail.com>
2024-05-03 01:50:33 +0300

    deploy fixed

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
-import glob from "glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/weather_app/",
-  build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    },
-},
+  base:'./'
 })
